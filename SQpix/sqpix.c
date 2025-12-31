8c8b9fa9083e7e9766def0d56bb620f30fa8f9e4
Samuel-DEVULDER <samuel.devulder@laposte.net>
2025-12-31 18:08:54 +0100

    fixed conversion

--- SQpix/sqpix.c
+++ SQpix/sqpix.c
@@ -8,16 +8,42 @@
 #include <math.h>
 
 #include "stb/stb_image.h"
-#include "stb/stb_image_resize2.h"
 #include "stb/stb_ds.h"
 
-#define PRIVATE static
+#define PRIVATE 
+//static
 
 PRIVATE uint8_t exo = 0, bayer = 0, verbose = 0;
 PRIVATE char *input_file, *output_file;
 
 typedef float vec3[3];
-typedef float vec4[4];
+
+PRIVATE vec3 *vec3_set(vec3 *t, float x, float y, float z) {
+	(*t)[0] = x;
+	(*t)[1] = y;
+	(*t)[2] = z;	
+	return t;
+}
+
+PRIVATE vec3 *vec3_madd(vec3 *t, vec3 *u, float k, vec3 *v) {
+	int i;
+	for(i=0; i<3; ++i) (*t)[i] = (*u)[i] + k*(*v)[i];
+	return t;
+}
+
+#define vec3_add(t, u, v) vec3_madd((t),(u),+1.0f,(v))
+#define vec3_sub(t, u, v) vec3_madd((t),(u),-1.0f,(v))
+
+PRIVATE vec3 *vec3_mul(vec3 *t, vec3 *u, vec3 *v) {
+	return vec3_set(t,
+		(*u)[1] * (*v)[2] - (*u)[2] * (*v)[1],
+		(*u)[2] * (*v)[0] - (*u)[0] * (*v)[2],
+		(*u)[0] * (*v)[1] - (*u)[1] * (*v)[0]);
+}
+
+PRIVATE float vec3_dot(vec3 *u, vec3 *v) {
+	return (*u)[0] * (*v)[0] + (*u)[1] * (*v)[1] + (*u)[2] * (*v)[2];
+}
 
 typedef struct color {
 	vec3	pt;
@@ -33,76 +59,60 @@ typedef struct tetra {
 } tetra;
 
 #define FULL	1.0f
-#define HALF	0.542574f
+#define HALF	0.542574163422f
 
 PRIVATE color palette[15];
 
+PRIVATE int float_cmp(float x, float y) {
+	return x<y ? -1 : x>y ? +1 : 0;
+}
+
 PRIVATE int color_cmp_by_intens(const void *pa, const void *pb) {
-	const color *a = pa, *b = pb;
-	return a->intens < b->intens ? -1 :
-	       a->intens > b->intens ? +1 : 0;	
+	const color * const *a = pa, * const *b = pb;
+	return float_cmp((*a)->intens, (*b)->intens);
 }
 
 PRIVATE int color_cmp_by_weight(const void *pa, const void *pb) {
-	const color *a = pa, *b = pb;
-	return a->weight > b->weight ? -1 :
-	       a->weight < b->weight ? +1 : 0;	
+	const color * const *a = pa, * const *b = pb;
+	return -float_cmp((*a)->weight, (*b)->weight);
 }
 
 PRIVATE void set_palette(int i, float r, float g, float b) {
 	color *c = &palette[i];
-	c->index = i;
-	c->pt[0] = r;
-	c->pt[1] = g;
-	c->pt[2] = b;
-	c->intens = .3f*r +.59f*g  + .11f*b;
+	vec3_set(&c->pt, r,g,b);
+	c->index  = i;
+	c->intens = 
+		.3f*r + .59f*g + .11f*b;
+		//.2126f*r +.7152f*g  + .0722f*b;
 }
 
-PRIVATE tetra tetras[28], *tetra_list;
-
-PRIVATE vec3 *vec3_madd(vec3 *t, vec3 *u, float k, vec3 *v) {
-	int i;
-	for(i=0; i<3; ++i) (*t)[i] = (*u)[i]+k*(*v)[i];
-	return t;
-}
-
-#define vec3_add(t, u, v) vec3_madd((t),(u),+1.0f,(v))
-#define vec3_sub(t, u, v) vec3_madd((t),(u),-1.0f,(v))
-
-PRIVATE vec3 *vec3_mul(vec3 *t, vec3 *u, vec3 *v) {
-	(*t)[0] = (*u)[1]*(*v)[2] - (*u)[2]*(*v)[1];
-	(*t)[1] = (*u)[2]*(*v)[0] - (*u)[0]*(*v)[2];
-	(*t)[2] = (*u)[0]*(*v)[1] - (*u)[1]*(*v)[0];
-	return t;
-}
-
-PRIVATE float vec3_dot(vec3 *u, vec3 *v) {
-	return (*u)[0]*(*v)[0] + (*u)[1]*(*v)[1] + (*u)[2]*(*v)[2];
-}
+PRIVATE tetra tetras[27], *tetra_list;
 
 PRIVATE void new_tetra(tetra *tetra, int a, int b, int c, int d) {
 	vec3 p01,p02,p03,p12,p13;
+	color **T = tetra->p;
 
-	tetra->p[0] = &palette[a];
-	tetra->p[1] = &palette[b];
-	tetra->p[2] = &palette[c];
-	tetra->p[3] = &palette[d];
+	T[0] = &palette[a];
+	T[1] = &palette[b];
+	T[2] = &palette[c];
+	T[3] = &palette[d];
 			
 	tetra->prev = NULL;
 	tetra->next = tetra_list;
-	if(tetra->next) tetra->next->prev = tetra;
+	if(tetra->next) 
+	tetra->next->prev = tetra;
+	tetra_list = tetra;
 	
-	vec3_sub(&p01, &tetra->p[1]->pt, &tetra->p[0]->pt);
-	vec3_sub(&p02, &tetra->p[2]->pt, &tetra->p[0]->pt);
-	vec3_sub(&p03, &tetra->p[3]->pt, &tetra->p[0]->pt);
-	vec3_sub(&p12, &tetra->p[2]->pt, &tetra->p[1]->pt);
-	vec3_sub(&p13, &tetra->p[3]->pt, &tetra->p[1]->pt);
+	vec3_sub(&p01, &T[1]->pt, &T[0]->pt);
+	vec3_sub(&p02, &T[2]->pt, &T[0]->pt);
+	vec3_sub(&p03, &T[3]->pt, &T[0]->pt);
+	vec3_sub(&p12, &T[2]->pt, &T[1]->pt);
+	vec3_sub(&p13, &T[3]->pt, &T[1]->pt);
 	
 	vec3_mul(&tetra->n012, &p01, &p02);
 	vec3_mul(&tetra->n023, &p02, &p03);
 	vec3_mul(&tetra->n031, &p03, &p01);
-	vec3_mul(&tetra->n023, &p02, &p03);
-	vec3_mul(&tetra->n132, &p13, &p03);
+	vec3_mul(&tetra->n132, &p13, &p12);
 	
 	assert(vec3_dot(&tetra->n012, &p03)>=0);
 }
@@ -113,7 +123,7 @@ PRIVATE void tetra_proj3(float *w0, float *w1, float *w2,
 	vec3 diff, edge0, edge1;
 	float a00,a01,a11,b0,b1,det,t0,t1;
 	
-	vec3_sub(&diff,  point, v0);
+	vec3_sub(&diff, point, v0);
 	vec3_sub(&edge0, v1, v0);
 	vec3_sub(&edge1, v2, v0);
 	
@@ -124,9 +134,9 @@ PRIVATE void tetra_proj3(float *w0, float *w1, float *w2,
 	b0 = -vec3_dot(&diff, &edge0);
 	b1 = -vec3_dot(&diff, &edge1);
 	
-	det = a00 * a11 - a01 * a01;
 	t0  = a01 * b1  - a11 * b0;
 	t1  = a01 * b0  - a00 * b1;
+	det = a00 * a11 - a01 * a01;
 	
 	if(t0 + t1 <= det) {
 		if(t0 < 0) {
@@ -185,7 +195,7 @@ PRIVATE void tetra_proj3(float *w0, float *w1, float *w2,
 	*w0 = 1-t0-t1; *w1 = t0; *w2 = t1;
 }
 
-PRIVATE void tetra_proj2(float *w0, float *w1, vec3 *point, vec3 *v0, vec3 *v1) {
+PRIVATE void tetra_proj2(float *w0, float *w1, vec3 *p, vec3 *v0, vec3 *v1) {
 	vec3 v10, q;
 	float t;
 	
@@ -195,8 +205,7 @@ PRIVATE void tetra_proj2(float *w0, float *w1, vec3 *point, vec3 *v0, vec3 *v1) 
 	if(t<=0) {
 		t = 0;
 	} else {
-		vec3_sub(&q, point, v0);
-		t = vec3_dot(&q,&v10)/t;
+		t = vec3_dot(vec3_sub(&q, p, v0),&v10)/t;
 		t = t<=0 ? 0 : t>1 ? 1 : t;
 	}
 	*w0 = 1-t; *w1 = t;
@@ -217,7 +226,15 @@ PRIVATE void tetra_coord(tetra *tetra, vec3 *p, vec3 *b) {
 		w0 = vec3_dot(&q, &tetra->n132);
 	} while(0);
 	
-	if(w0 < 0) {
+	// if(w0<=0 && w1<=0 && w2<=0 && w3<=0) {
+		// w0 = -w0; w1 = -w1; 
+		// w2 = -w2; w3 = -w3;
+	// }
+	if(w0>=0 && w1>=0 && w2>=0 && w3>=0) {
+		float  tot = w0 + w1 + w2 + w3;
+		tot = tot<=0 ? 0 : 1.0f/tot;
+		w0 *= tot; w1 *= tot; w2 *= tot; w3 *= tot;
+	} else if(w0 < 0) {
 		if(w1 < 0) {
 			if(w2 < 0) {
 				w3 = 1;
@@ -272,23 +289,19 @@ PRIVATE void tetra_coord(tetra *tetra, vec3 *p, vec3 *b) {
 		tetra_proj3(&w0, &w1, &w2, p, &T[0]->pt, &T[1]->pt, &T[2]->pt);
 		w3 = 0;
 	}
-
-	do { 	float  tot = w0 + w1 + w2 + w3;
-		tot = tot<=0 ? 0 : 1.0f/tot;
 	
-		w0 *= tot; w1 *= tot; w2 *= tot; w3 *= tot;
-	} while(0);
-	
-	tetra->p[0]->weight = w0;
-	tetra->p[1]->weight = w1;
-	tetra->p[2]->weight = w2;
-	tetra->p[3]->weight = w3;
+	T[0]->weight = w0;
+	T[1]->weight = w1;
+	T[2]->weight = w2;
+	T[3]->weight = w3;
 	
 	if(b != NULL) {
 		int i;
 		for(i=0;i<3;++i) {
-			(*b)[i] = w0*T[0]->pt[i] + w1*T[1]->pt[i]
+			float x = w0*T[0]->pt[i] + w1*T[1]->pt[i]
 		                + w2*T[2]->pt[i] + w3*T[3]->pt[i];
+			
+			(*b)[i] = x<0.0f ? 0.0f : x>1.0f ? 1.0f : x;
 		}
 	}
 }
@@ -316,10 +329,10 @@ uint8_t dith_bayer[8][8] = {
 };
 
 PRIVATE uint32_t dith_key(vec3 *p) {
-	return  (uint32_t)(24*powf((*p)[0]<0?0:(*p)[0],0.45f)) +
-		(uint32_t)(24*powf((*p)[1]<0?0:(*p)[1],0.45f))*32 +
-		(uint32_t)(24*powf((*p)[2]<0?0:(*p)[2],0.45f))*32*32;
-}
+	return  (uint32_t)(24*powf((*p)[0]<=0 ? 0 : (*p)[0], 0.45f)) +
+		(uint32_t)(24*powf((*p)[1]<=0 ? 0 : (*p)[1], 0.45f))*32 +
+		(uint32_t)(24*powf((*p)[2]<=0 ? 0 : (*p)[2], 0.45f))*32*32;
+ }
 
 PRIVATE struct dith_cache {
 	uint32_t key;
@@ -330,13 +343,14 @@ PRIVATE tetra *dith_find_tetra(vec3 *p) {
 	float w0 = 0, w1 = 0, w2 = 0, w3 = 0;
 	float  best_d = FLT_MAX;
 	tetra *best_t = NULL, *t;
-	
+
 	for(t = tetra_list; t; t = t->next) {
 		float d; vec3 q;
-	
+
 		tetra_coord(t, p, &q);
-		d = vec3_dot(&q, &q);
 		
+		vec3_sub(&q, &q, p);
+		d = vec3_dot(&q, &q);
 		if(d < best_d) {
 			best_d = d;
 			best_t = t;
@@ -347,14 +361,16 @@ PRIVATE tetra *dith_find_tetra(vec3 *p) {
 			if(d <= 1e-5) break; /* shortcut */
 		}
 	}
-	
+
 	/* move found to first place.
 	   idea here is to have an LRU organisation
 	 */
 	if(best_t != tetra_list) {
+		if(best_t->next) 
 		best_t->next->prev = best_t->prev;
 		best_t->prev->next = best_t->next;
 		
+		tetra_list->prev = best_t;
 		best_t->next = tetra_list;
 		best_t->prev = NULL;
 		
@@ -369,7 +385,7 @@ PRIVATE tetra *dith_find_tetra(vec3 *p) {
 	return best_t;
 }
 
-#define lenof(array) (sizeof(array)/sizeof(array[0]))
+#define length_of(array) (sizeof(array)/sizeof(array[0]))
 
 PRIVATE uint8_t dith(const uint8_t *dith, const int mx, const int my,  
 		     const int x, const int y, vec3 *p) {
@@ -377,14 +393,22 @@ PRIVATE uint8_t dith(const uint8_t *dith, const int mx, const int my,
 	struct dith_cache *cache = hmgetp_null(dith_cache, key);
 	
 	const int mxy = mx*my;
-	assert(mxy <= lenof(cache->value));
+	assert(mxy <= length_of(cache->value));
 
 	if(cache == NULL) {
 		tetra *t = dith_find_tetra(p);
 		color *sel[4] = {t->p[0], t->p[1], t->p[2], t->p[3]},
-		      *tab[lenof(cache->value)];
-		
+		      *tab[length_of(cache->value)];
+		      	
 		qsort(sel, 4, sizeof(sel[0]), color_cmp_by_weight);
+		// printf("%g %g %g %g\n", sel[0]->weight,sel[1]->weight,sel[2]->weight,sel[3]->weight);
+		// printf("%d %d %d %d\n", sel[0]->index,sel[1]->index,sel[2]->index,sel[3]->index);
+		// vec3 <q; 
+		// tetra_coord(t, p, &q);
+		// printf("%g %g %g\n", (*p)[0], (*p)[1], (*p)[2]);
+		// printf("%g %g %g\n", q[0], q[1], q[2]);
+		// vec3_sub(&q,&q,p);
+		// printf("%g\n", vec3_dot(&q,&q));
 		
 		do {
 			int i = 0; float m = 0;			
@@ -405,6 +429,10 @@ PRIVATE uint8_t dith(const uint8_t *dith, const int mx, const int my,
 		
 		cache = hmgetp_null(dith_cache, key);
 		assert(cache != NULL);
+
+		// for(int i=0;i<64;++i) printf("%d ", cache->value[i]);
+		// printf("\n");
+		// exit(0);
 	}
 	
 	return cache->value[dith[(x % mx)*my + (y % my)]-1];
@@ -415,20 +443,45 @@ typedef struct {
 	int h;
 	uint8_t *sRGB;
 	uint8_t bitmap[65536];
-	struct timeval start;
+	struct timeval time;
 } pic;
+
+PRIVATE float pic_done(pic *pic) {
+	struct timeval now;
+	float secs = 0;
+	
+	if(pic->sRGB) {
+		free(pic->sRGB);
+		pic->sRGB = NULL;
+	
+		gettimeofday(&now, NULL);
+		if(now.tv_usec<pic->time.tv_usec) {
+			now.tv_usec += 1000000;
+			now.tv_sec--;
+		}
+		pic->time.tv_sec  = now.tv_sec  - pic->time.tv_sec;
+		pic->time.tv_usec = now.tv_usec - pic->time.tv_usec;
+		secs = pic->time.tv_sec + pic->time.tv_usec/1000000.0f;
+	}
+		
+	if(verbose) {
+		printf("done (%.1fms)\n", secs*1000.0f);
+	}
+	
+	return secs;
+}
 
 PRIVATE void pic_load(pic *pic, const char *filename) {
 	int n;
 	
-	gettimeofday(&pic->start, NULL);
+	gettimeofday(&pic->time, NULL);
 	
 	if(!stbi_info(filename, &pic->w, &pic->h, &n)) {
 		fprintf(stderr, "Unsupported image: %s\n", filename);
 		exit(-1);
 	}
 	if(verbose) {
-		printf("%s (%d x %d x %d)...", filename, pic->w, pic->h, n);
+		printf("%s (%dx%d)...", filename, pic->w, pic->h);
 		fflush(stdout);
 	}
 	pic->sRGB = stbi_load(filename, &pic->w, &pic->h, &n, 3);
@@ -447,6 +500,14 @@ PRIVATE void pic_save(pic *pic, const char *filename) {
 		return;
 	}
 	
+	if(verbose) {
+		const char *s = filename;
+		while(*s) ++s;
+		while(s>filename && *s!='/' && *s!='\\') --s;
+		printf("saving (%s)...", s);
+		fflush(stdout);
+	}
+	
 	fprintf(f, exo ? "SQP\2" : "SQP\1");
 	
 	if(exo) {
@@ -459,16 +520,26 @@ PRIVATE void pic_save(pic *pic, const char *filename) {
 	}
 	fflush(f);
 	fclose(f);
+}
+
+PRIVATE void pic_save_pgm(pic *pic, const char *filename) {
+	FILE *f = fopen(filename, "wt");
+	int i;
 	
-	if(verbose) {
-		struct timeval stop;
-		float us;
-		
-		gettimeofday(&stop, NULL);
-		us = (stop.tv_sec  - pic->start.tv_sec) * 1000000.0f + 
-		      stop.tv_usec - pic->start.tv_usec;
-		printf("done (%.1fms)\n", us/1000.0);
+	if(f==NULL) {
+		perror(filename);
+		return;
 	}
+	
+	fprintf(f, "P3\n256 256\n255\n");
+	for(i=0;i<65536;++i) {
+		int c = pic->bitmap[i]>=8 ? 187 : 255;
+		fprintf(f, "%d %d %d\n", 
+			pic->bitmap[i] & 4 ? 0 : c,
+			pic->bitmap[i] & 2 ? 0 : c,
+			pic->bitmap[i] & 1 ? 0 : c);
+	}
+	fclose(f);
 }
 
 PRIVATE vec3 *pic_get_linear_color(pic *pic, int x, int y, vec3 *ret) {
@@ -477,25 +548,26 @@ PRIVATE vec3 *pic_get_linear_color(pic *pic, int x, int y, vec3 *ret) {
 	if(sRGB[255]==0) {
 		int i;
 		for(i=0;i<256;++i) {
-			float x = i/255;
+			float x = i/255.0f;
 			sRGB[i] = x<=0.04045f ? x/12.92f : powf((x+0.055f)/1.055f, 2.4f);
 		}
 	}
 	
 	if(x<0 || y<0 || x>=pic->w || y>=pic->h) {
-		(*ret)[0] = (*ret)[1] = (*ret)[2] = 0;
+		vec3_set(ret, 0,0,0);
 	} else {
 		uint8_t *img = pic->sRGB + 3*(pic->w*y + x);
-		(*ret)[0] = sRGB[img[0]];
-		(*ret)[1] = sRGB[img[1]];
-		(*ret)[2] = sRGB[img[2]];
+		vec3_set(ret, sRGB[img[0]], sRGB[img[1]], sRGB[img[2]]);
 	}
+	
+	// vec3_set(ret, sRGB[112], sRGB[23], sRGB[25]);
+	// vec3_set(ret, 0.20792611981001, 0.02084547344562, 0.022794021361973);
 	
 	return ret;
 }
 
 PRIVATE void squale_coord(pic *pic, int x, int y, int *rx, int *ry) {
-	int w=pic->w, h=pic->h;
+	const int w = pic->w, h = pic->h;
 	if(w<=h) {
 		*rx = (x*h + 128*(w - h))/256;
 		*ry = (y*h)/256;
@@ -512,23 +584,28 @@ PRIVATE vec3 *squale_color(pic *pic, int x, int y, vec3 *ret) {
 	squale_coord(pic, x-1,y-1, &x1, &y1);
 	squale_coord(pic, x+1,y+1, &x2, &y2);
 	
-	k = 1.0f/(1.0f+y2-y1)*(1.0f+x2-x1);
+	k = 1.0f/((1.0f+y2-y1)*(1.0f+x2-x1));
 	
-	(*ret)[0] = (*ret)[1] = (*ret)[2] = 0;
+	vec3_set(ret, 0,0,0);
 	for(i=x1; i<=x2; ++i) for(j = y1; j<=y2; ++j) {
 		vec3_madd(ret, ret, k, pic_get_linear_color(pic, i,j, &p));
 	}
 	
+	// (*ret)[0] = x/255.0f;	
+	// (*ret)[1] = y/255.0f;	
+			
 	return ret;
 }
 
 PRIVATE void pic_dither(pic *pic, int x, int y) {
 	vec3 p; 
-	
+
+	x &= 255; y &= 255;
+
 	int8_t c = dith(
-	        bayer ? &dith_bayer[0][0]    : &dith_vac[0][0], 
-		bayer ? lenof(dith_bayer)    : lenof(dith_vac),
-		bayer ? lenof(dith_bayer[0]) : lenof(dith_vac[0]),
+	        bayer ? &dith_bayer[0][0]        : &dith_vac[0][0], 
+		bayer ? length_of(dith_bayer)    : length_of(dith_vac),
+		bayer ? length_of(dith_bayer[0]) : length_of(dith_vac[0]),
 		x, y, squale_color(pic, x, y, &p)
 	);
 	
@@ -536,21 +613,25 @@ PRIVATE void pic_dither(pic *pic, int x, int y) {
 }
 
 PRIVATE void pic_conv_h(pic *pic) {
-	static int dir[] = {256,1,-256,-1}, a, p, l = 8, b = 1;
+	static int dir[] = {256,1,-256,-1}, a, p = 0, l = 8, b = 1;
 	if(pic==NULL)  {
 		l = 8;
 		b = 1;
+		p = 0;
 	} else if(l == 0) {
 		pic_dither(pic, p & 255, p>>8);
 	} else {
 		--l;
-		a -= (b=-b); pic_conv_h(pic);
+		a -= (b=-b); 
+		pic_conv_h(pic);
 		p = p + dir[a&3]; 
-		a -= (b=-b); pic_conv_h(pic);
-		p = p + dir[a&3]; pic_conv_h(pic);
-		p = p + dir[a&3]; pic_conv_h(pic);
+		a -= (b=-b); 
+		pic_conv_h(pic);
+		p = p + dir[a&3]; 
+		pic_conv_h(pic);
 		a += (b=-b); 
-		p = p + dir[a&3]; pic_conv_h(pic); 
+		p = p + dir[a&3]; 
+		pic_conv_h(pic); 
 		a += (b=-b);		
 		++l;
 	}
@@ -562,52 +643,31 @@ PRIVATE void pic_conv_l(pic *pic) {
 }
 
 PRIVATE void init(void) {
+	static uint16_t tetras_desc[] = {
+		 0x0518, 0x0458, 0x0248, 0x4268, 
+		 0x0328, 0x0138, 0x1389, 0x5189, 
+		 0x682A, 0x328A, 0x3A8B, 0x8A7B, 
+		 0x879B, 0x389B, 0x584C, 0x486C, 
+		 0x87CD, 0x58CD, 0x897D, 0x598D, 
+		 0x78CE, 0x6C8E, 0x68AE, 0x7A8E
+	};
+	int i;
+	
 	stbds_rand_seed(time(0));	
+	
+	for(i=0; i<15; ++i) {
+		float c = i>=8 ? HALF : FULL;
+		set_palette(i, i&4 ? 0.0f : c,
+		               i&2 ? 0.0f : c,
+			       i&1 ? 0.0f : c);
+	}
 
-	set_palette( 0, FULL*1, FULL*1, FULL*1);
-	set_palette( 1, FULL*1, FULL*1, FULL*0);
-	set_palette( 2, FULL*1, FULL*0, FULL*1);
-	set_palette( 3, FULL*1, FULL*0, FULL*0);
-	set_palette( 4, FULL*0, FULL*1, FULL*1);
-	set_palette( 5, FULL*0, FULL*1, FULL*0);
-	set_palette( 6, FULL*0, FULL*0, FULL*1);
-	set_palette( 7, FULL*0, FULL*0, FULL*0);
-	set_palette( 8, HALF*1, HALF*1, HALF*1);
-	set_palette( 9, HALF*1, HALF*1, HALF*0);
-	set_palette(10, HALF*1, HALF*0, HALF*1);
-	set_palette(11, HALF*1, HALF*0, HALF*0);
-	set_palette(12, HALF*0, HALF*1, HALF*1);
-	set_palette(13, HALF*0, HALF*1, HALF*0);
-	set_palette(14, HALF*0, HALF*0, HALF*1);
-
-	new_tetra(&tetras[ 0], 0, 4, 1, 5);
-	new_tetra(&tetras[ 1], 0, 2, 4, 6);
-	new_tetra(&tetras[ 2], 0, 4, 5, 8);
-	new_tetra(&tetras[ 3], 0, 6, 4, 8);
-	new_tetra(&tetras[ 4], 0, 1, 3, 8);
-	new_tetra(&tetras[ 5], 0, 2, 6, 8);
-	new_tetra(&tetras[ 6], 0, 3, 2, 8);
-	new_tetra(&tetras[ 7], 0, 5, 1, 8);
-	new_tetra(&tetras[ 8], 3, 8, 1, 9);
-	new_tetra(&tetras[ 9], 5, 1, 8, 9);
-	new_tetra(&tetras[10], 3, 2, 8,10);
-	new_tetra(&tetras[11], 6, 8, 2,10);
-	new_tetra(&tetras[12], 7, 9, 8,10);
-	new_tetra(&tetras[13], 3, 8, 9,10);
-	new_tetra(&tetras[14], 3,10, 9,11);
-	new_tetra(&tetras[15], 7, 9,10,11);
-	new_tetra(&tetras[16], 5, 8, 4,12);
-	new_tetra(&tetras[17], 7, 8, 9,12);
-	new_tetra(&tetras[18], 7,10, 8,12);
-	new_tetra(&tetras[19], 4, 8, 6,12);
-	new_tetra(&tetras[20], 5, 8,12,13);
-	new_tetra(&tetras[21], 5, 9, 8,13);
-	new_tetra(&tetras[22], 7,12, 9,13);
-	new_tetra(&tetras[23], 9,12, 8,13);
-	new_tetra(&tetras[24], 8,12,10,14);
-	new_tetra(&tetras[25], 6, 8,10,14);
-	new_tetra(&tetras[26],10,12, 7,14);
-	new_tetra(&tetras[27], 6,12, 8,14);
+	for(i=0; i<length_of(tetras_desc); ++i) {
+		uint16_t x = tetras_desc[i];
+		new_tetra(&tetras[i], 
+			  x>>12,(x>>8)&15,
+			  (x>>4)&15,x&15);
+	}
 }
 
 PRIVATE void usage(char *av0) {
@@ -627,6 +687,8 @@ PRIVATE void parse(int ac, char **av) {
 		|| !strcmp("-h", av[i])
 		|| !strcmp("--help", av[i])
 		|| 0) usage(av[0]);
+		else if(!strcmp("-v", av[i])) 
+			verbose = 1;
 		else if(!strcmp("-x", av[i])) 
 			exo = 1;
 		else if(!strcmp("-b", av[i])) 
@@ -646,7 +708,7 @@ PRIVATE void parse(int ac, char **av) {
 		exit(-1);
 	}
 
-	if(input_file == NULL) {
+	if(output_file == NULL) {
 		fprintf(stderr, "Missing output file\n");
 		exit(-1);
 	}
@@ -660,8 +722,11 @@ int main(int ac, char **av) {
 		pic pic;
 		
 		pic_load(&pic, input_file);
-		pic_conv_l(&pic);
+		pic_conv_h(NULL);
+		pic_conv_h(&pic);
+		pic_save_pgm(&pic, "toto.pgm");
 		pic_save(&pic, output_file);
+		pic_done(&pic);
 	} while(0);
 	
 	return 0;
